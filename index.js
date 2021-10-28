877042486fd43b59a11aa283be88a00db1e28fd2
Tirth <teerthshingala08@gmail.com>
2021-10-29 00:59:46 +0530

    fixed

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const http = require("http");
 const httpServer = http.createServer(app);
 const { Server } = require("socket.io");
 const cors = require("cors");
-require("dotenv").config();
 
 app.use(cors());
 
